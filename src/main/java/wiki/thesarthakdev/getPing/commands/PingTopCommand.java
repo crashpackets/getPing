df90853565a188d94d0038584e9e6245c016e04b
TheSarthakDev <personal@thesarthakdev.wiki>
2025-02-03 14:40:58 +0530

    Updated the code

--- src/main/java/wiki/thesarthakdev/getPing/commands/PingTopCommand.java
+++ src/main/java/wiki/thesarthakdev/getPing/commands/PingTopCommand.java
@@ -1,8 +1,6 @@
 package wiki.thesarthakdev.getPing.commands;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,42 +8,29 @@ import org.bukkit.entity.Player;
 import wiki.thesarthakdev.getPing.getPing;
 import wiki.thesarthakdev.getPing.utils.LeaderboardUtils;
 
-import java.util.List;
-
 public class PingTopCommand implements CommandExecutor {
     private final getPing plugin;
-    private final MiniMessage miniMessage;
 
     public PingTopCommand(getPing plugin) {
         this.plugin = plugin;
-        this.miniMessage = MiniMessage.miniMessage();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(miniMessage.deserialize(plugin.getConfig().getString("messages.console_error", "<red>Only players can use this command!")));
+            sender.sendMessage(plugin.getConfig().getString("messages.console_error", "<red>Only players can use this command!"));
             return true;
         }
 
         Player player = (Player) sender;
         if (!player.hasPermission("getping.pingtop")) {
-            player.sendMessage(miniMessage.deserialize(plugin.getConfig().getString("messages.no_permission", "<red>You do not have permission to use this command!")));
+            player.sendMessage(plugin.getConfig().getString("messages.no_permission", "<red>You do not have permission to use this command!"));
             return true;
         }
 
-        // Get the leaderboard
-        List<Player> leaderboard = LeaderboardUtils.getPingLeaderboard(plugin);
-
-        // Display the leaderboard
-        Component message = miniMessage.deserialize("<green>Top 10 Players with Best Ping:</green>\n");
-        for (int i = 0; i < Math.min(leaderboard.size(), 10); i++) {
-            Player target = leaderboard.get(i);
-            int ping = target.getPing();
-            message = message.append(miniMessage.deserialize("<green>" + (i + 1) + ". " + target.getName() + ": <color>" + ping + "ms</color>\n"));
-        }
-
-        player.sendMessage(message);
+        // Get and display the leaderboard
+        Component leaderboard = LeaderboardUtils.getLeaderboard(plugin);
+        player.sendMessage(leaderboard);
         return true;
     }
 }
--- src/main/java/wiki/thesarthakdev/getPing/utils/LeaderboardUtils.java
+++ src/main/java/wiki/thesarthakdev/getPing/utils/LeaderboardUtils.java
@@ -1,18 +1,41 @@
 package wiki.thesarthakdev.getPing.utils;
 
-import wiki.thesarthakdev.getPing.getPing;
-
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import wiki.thesarthakdev.getPing.getPing;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LeaderboardUtils {
-    public static List<Player> getPingLeaderboard(getPing plugin) {
-        return Bukkit.getOnlinePlayers().stream()
+    private static final MiniMessage miniMessage = MiniMessage.miniMessage();
+
+    public static Component getLeaderboard(getPing plugin) {
+        // Get leaderboard settings from config
+        String title = plugin.getConfig().getString("leaderboard.title", "<green>Top %count% Players with Best Ping:</green>");
+        String entryFormat = plugin.getConfig().getString("leaderboard.entry_format", "<green>%position%. %player%: <color>%ping%ms</color>");
+        int maxEntries = plugin.getConfig().getInt("leaderboard.max_entries", 10);
+
+        // Get the leaderboard
+        List<Player> leaderboard = Bukkit.getOnlinePlayers().stream()
                 .sorted(Comparator.comparingInt(Player::getPing))
                 .collect(Collectors.toList());
+
+        // Build the leaderboard message
+        Component message = miniMessage.deserialize(title.replace("%count%", String.valueOf(maxEntries)) + "\n");
+        for (int i = 0; i < Math.min(leaderboard.size(), maxEntries); i++) {
+            Player target = leaderboard.get(i);
+            int ping = target.getPing();
+            String entry = entryFormat
+                    .replace("%position%", String.valueOf(i + 1))
+                    .replace("%player%", target.getName())
+                    .replace("%ping%", String.valueOf(ping));
+            message = message.append(miniMessage.deserialize(entry + "\n"));
+        }
+
+        return message;
     }
 }
